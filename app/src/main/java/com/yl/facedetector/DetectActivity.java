b55f2aee611a232064a424f862623a1013ff7fa0
Yangli <yangl7240@gmail.com>
2017-09-22 14:23:16 +0800

    设置摄像机的属性，包括全屏等

--- app/src/main/java/com/yl/facedetector/DetectActivity.java
+++ app/src/main/java/com/yl/facedetector/DetectActivity.java
@@ -1,16 +1,15 @@
 package com.yl.facedetector;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
 
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
@@ -27,25 +26,11 @@ import java.io.InputStream;
 public class DetectActivity extends AppCompatActivity implements
         CvCameraViewListener2 {
 
-    private static final String TAG = "OCVSample::Activity";
     private static final Scalar FACE_RECT_COLOR = new Scalar(0, 255, 0, 255);
-    public static final int JAVA_DETECTOR = 0;
-    public static final int NATIVE_DETECTOR = 1;
-
-    private MenuItem mItemFace50;
-    private MenuItem mItemFace40;
-    private MenuItem mItemFace30;
-    private MenuItem mItemFace20;
-    private MenuItem mItemType;
-
     private Mat mRgba;
     private Mat mGray;
     private File mCascadeFile;
     private CascadeClassifier mJavaDetector;
-
-    private int mDetectorType = JAVA_DETECTOR;
-    private String[] mDetectorName;
-
     private float mRelativeFaceSize = 0.2f;
     private int mAbsoluteFaceSize = 0;
 
@@ -55,39 +40,37 @@ public class DetectActivity extends AppCompatActivity implements
         System.loadLibrary("opencv_java3");
     }
 
-    public DetectActivity() {
-        mDetectorName = new String[2];
-        mDetectorName[JAVA_DETECTOR] = "Java";
-        mDetectorName[NATIVE_DETECTOR] = "Native (tracking)";
-
-        Log.i(TAG, "Instantiated new " + this.getClass());
-    }
-
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        configWindowSetting();
         setContentView(R.layout.activty_detect);
 
         mOpenCvCameraView = (CameraBridgeViewBase)
                 findViewById(R.id.fd_activity_surface_view);
         mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        // 切换前置摄像
+        mOpenCvCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_FRONT);
         mOpenCvCameraView.setCvCameraViewListener(this);
 
         init();
     }
 
+    // 配置摄像机，全屏、横屏和常亮
+    private void configWindowSetting() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
     private void init() {
         try {
-            // load cascade file from application resources
-            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+            InputStream is = getResources()
+                    .openRawResource(R.raw.lbpcascade_frontalface);
             File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
             mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
             FileOutputStream os = new FileOutputStream(mCascadeFile);
-
             byte[] buffer = new byte[4096];
             int bytesRead;
             while ((bytesRead = is.read(buffer)) != -1) {
@@ -95,20 +78,20 @@ public class DetectActivity extends AppCompatActivity implements
             }
             is.close();
             os.close();
-
             mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
             if (mJavaDetector.empty()) {
-                Log.e(TAG, "Failed to load cascade classifier");
                 mJavaDetector = null;
             }
             cascadeDir.delete();
-
         } catch (IOException e) {
             e.printStackTrace();
-            Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
         }
-
         mOpenCvCameraView.enableView();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
     }
 
     @Override
@@ -116,11 +99,6 @@ public class DetectActivity extends AppCompatActivity implements
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
     }
 
     public void onDestroy() {
@@ -139,9 +117,11 @@ public class DetectActivity extends AppCompatActivity implements
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-
         mRgba = inputFrame.rgba();
         mGray = inputFrame.gray();
+        // 翻转矩阵以适应前置摄像头
+        Core.flip(mRgba, mRgba, 1);
+        Core.flip(mGray, mGray, 1);
 
         if (mAbsoluteFaceSize == 0) {
             int height = mGray.rows();
@@ -149,64 +129,15 @@ public class DetectActivity extends AppCompatActivity implements
                 mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
             }
         }
-
         MatOfRect faces = new MatOfRect();
-
-        if (mDetectorType == JAVA_DETECTOR) {
-            if (mJavaDetector != null)
-                mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2,
-                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
-        } else {
-            Log.e(TAG, "Detection method is not selected!");
+        if (mJavaDetector != null) {
+            mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2,
+                    new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
         }
-
         Rect[] facesArray = faces.toArray();
         for (int i = 0; i < facesArray.length; i++)
             Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(),
                     FACE_RECT_COLOR, 3);
-
         return mRgba;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        Log.i(TAG, "called onCreateOptionsMenu");
-        mItemFace50 = menu.add("Face size 50%");
-        mItemFace40 = menu.add("Face size 40%");
-        mItemFace30 = menu.add("Face size 30%");
-        mItemFace20 = menu.add("Face size 20%");
-        mItemType = menu.add(mDetectorName[mDetectorType]);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
-        if (item == mItemFace50)
-            setMinFaceSize(0.5f);
-        else if (item == mItemFace40)
-            setMinFaceSize(0.4f);
-        else if (item == mItemFace30)
-            setMinFaceSize(0.3f);
-        else if (item == mItemFace20)
-            setMinFaceSize(0.2f);
-        else if (item == mItemType) {
-            int tmpDetectorType = (mDetectorType + 1) % mDetectorName.length;
-            item.setTitle(mDetectorName[tmpDetectorType]);
-            setDetectorType(tmpDetectorType);
-        }
-        return true;
-    }
-
-    private void setMinFaceSize(float faceSize) {
-        mRelativeFaceSize = faceSize;
-        mAbsoluteFaceSize = 0;
-    }
-
-    private void setDetectorType(int type) {
-        if (mDetectorType != type) {
-            mDetectorType = type;
-            Log.i(TAG, "Cascade detector enabled");
-        }
     }
 }
